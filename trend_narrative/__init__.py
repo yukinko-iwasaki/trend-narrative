257bb678439127943e35305d85ab187268f7d24b
Wei Lu <luwei.here@gmail.com>
2026-03-05 15:22:07 -0500

    Add new relationship narrative to module export

--- trend_narrative/__init__.py
+++ trend_narrative/__init__.py
@@ -35,12 +35,14 @@ from .narrative import (
     get_segment_narrative,
     millify,
 )
+from .relationship import get_relationship_narrative
 
 __all__ = [
     "TrendDetector",
     "InsightExtractor",
     "consolidate_segments",
     "get_segment_narrative",
+    "get_relationship_narrative",
     "millify",
 ]
 
